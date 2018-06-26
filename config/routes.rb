7f6173cb405b71996f5939448d6de24a9374ee3c
Qingsong <nileexpedition11@hotmail.com>
2018-06-26 20:25:22 +0800

    CRUD commit

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ Rails.application.routes.draw do
   get "welcome/index"
   get "articles/new"
 
-  resources :articles
-  
+  resources :articles do
+    resources :comments
+  end
   root "welcome#index"
 end
